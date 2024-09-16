cb02caeaa0fc6633f0628fc23280ff06c928b100
杨宇涛 <2794093098@qq.com>
2024-09-17 00:46:28 +0800

    feat: 4-6 add TabView

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+
+// 常规引入Vant组件样式
+import 'vant/lib/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// 引入底部导航栏组件
+import { Tabbar, TabbarItem } from 'vant'
+import { ref } from 'vue'
+
+//记录当前选择的tab,页面加载默认定位到home
+const active = ref('home')
+</script>
+
+<template>
+  <div class="center">{{ active }}</div>
+
+  <Tabbar v-model="active">
+    <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+    <TabbarItem name="order" icon="bars">订单</TabbarItem>
+    <TabbarItem name="me" icon="contact">我的</TabbarItem>
+  </Tabbar>
+</template>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+</style>
